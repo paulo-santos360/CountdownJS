72c2c00e57354e94f33e2a1e9e61a99e2975b682
Paulo Santos <102436341+paulo-santos360@users.noreply.github.com>
2023-09-11 10:33:54 -0300

    Javascript Date

--- js/app.js
+++ js/app.js
@@ -27,8 +27,8 @@ const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 //console.log(items);
 
-let futureDate = new Date(2023, 8, 11, 11, 30, 0);
-console.log(futureDate)
+let futureDate = new Date(2023, 8, 24, 11, 30, 0);//(2023, 8, 24, 11, 30, 0)
+//console.log(futureDate)
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -43,7 +43,7 @@ const weekday = weekdays[futureDate.getDay()];
 //const weekday = futureDate.getDay();
 //console.log(weekday);
 
-giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}`;
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am`;
 
 // future time is
 const futureTime = futureDate.getTime();
@@ -55,7 +55,7 @@ function getRemainingTime(){
     //console.log(today)
 
     const t = futureTime - today;
-    console.log(t)
+    //console.log(t)
 
     // 1s = 1000ms
     // 1m = 60s
@@ -81,6 +81,24 @@ function getRemainingTime(){
     
     // set values array;
     const values = [days, hours, minutes, seconds];
+
+    function format(item){
+        if(item < 10){
+            return item = `0${item}`
+        }
+        return item
+    }
+
+    items.forEach(function (item, index) {
+        item.innerHTML = format(values[index]);
+    });
+    if(t < 0){
+        clearInterval(countdown);
+        deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`
+    }
 }
+
+// countdown
+let countdown = setInterval(getRemainingTime, 1000)
 
 getRemainingTime();
